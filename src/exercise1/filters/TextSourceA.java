af286341b1392b192e989b085a571ce35dc51160
Michelle <michelle.weber@students.fhv.at>
2017-10-27 12:13:15 +0200

    read lines only

--- src/exercise1/filters/TextSourceA.java
+++ src/exercise1/filters/TextSourceA.java
@@ -3,25 +3,23 @@ package exercise1.filters;
 import pmp.filter.Source;
 
 import java.io.*;
-import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Michelle on 27.10.2017.
  */
-public class TextSourceA extends Source<List<String[]>> {
+public class TextSourceA extends Source<List<String>> {
     String _filename;
 
     @Override
-    public List<String[]> read() throws StreamCorruptedException {
-        List<String[]> lines = new ArrayList<>();
+    public List<String> read() throws StreamCorruptedException {
+        List<String> lines = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(_filename))));
             String line;
             while ((line = br.readLine()) != null) {
-                String[] words = line.split(" ");
-                lines.add(words);
+                lines.add(line);
             }
             br.close();
         } catch (FileNotFoundException e) {
