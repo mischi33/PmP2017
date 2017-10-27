c927c6d9fe24a9e5a63d94478115be98b71e7959
Flowkii <florian.braun@students.fhv.at>
2017-10-27 17:20:51 +0200

    Merge branch 'master' of /Users/Flo/Documents/FHV/IdeaProjects/WebServer/PmP2017_1 with conflicts.

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -11,6 +11,14 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
+        TextSourceA textSourceA = new TextSourceA("test.txt");
+        SimplePipe<List<String>> simplePipe = new SimplePipe<>(textSourceA);
+
+        RemoveSpecialChars removeSpecialChars = new RemoveSpecialChars(textSourceA);
+        LinesToWords linesToWords = new LinesToWords(removeSpecialChars);
+        CircularShifts circularShifts = new CircularShifts(linesToWords);
+        UselessWordsFilter uselessWordsFilter = new UselessWordsFilter(circularShifts);
+        SortFilter sortFilter = new SortFilter(uselessWordsFilter);
 
     }
 }
